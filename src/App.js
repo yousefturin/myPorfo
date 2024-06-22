98cf035cd550e7380f30ae61c979063e18f1dc56
yousefturin <yusefturin@gmail.com>
2024-06-22 18:46:01 +0300

    chore: Update CSS variables, remove unused code, and improve component styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import HeroSection from './components/hero/HeroSection';
 import LatestProject from './components/latestProject/LatestProject';
 import ProjectsCardsSection from './components/project/ProjectsCardsSection';
-// import EducationSection from './components/education/EducationSection';
+
 import SkillSection from './components/skills/SkillSection';
-// import EmployeeSection from './components/employee/EmployeeSection';
 import MenuBarSection from './components/menu/MenuBarSection';
 import FooterSection from './components/footer/footerSection';
-// import Banner from './components/banner/banner';
 import React from 'react';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
@@ -54,13 +52,10 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Banner /> */}
       <MenuBarSection btnDownloadCVPopUpRef={btnDownloadCVPopUpRef} />
       <HeroSection scrollToProjects={scrollToProjects} />
       <LatestProject projectSectionRef={projectSectionRef} />
       <ProjectsCardsSection />
-      {/* <EmployeeSection /> */}
-      {/* <EducationSection /> */}
       <SkillSection />
       <FooterSection />
       <ScrollToTop btnScrollToTopRef={btnScrollToTopRef} />
