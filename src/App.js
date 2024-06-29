77ac9b593acc226025bdcecdae25c0fb643d776f
yousefturin <yusefturin@gmail.com>
2024-06-29 16:08:04 +0300

    theme function is created

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Home from './components/Home/Home';
 import BlogPost from './components/BlogPost/BlogPost';
 import FooterSection from './components/footer/footerSection';
 import RestView from './components/ResetView/ResetView';
+// import { useTheme } from './context/ThemeProvider';
 
 
 function App() {
+  // const { theme, toggleTheme } = useTheme();
   return (
     <div className="App">
       <Router>
@@ -18,6 +20,12 @@ function App() {
           <Route path="/blog/:postId" element={<BlogPost />} />
         </Routes>
         <FooterSection />
+        {/* <div>
+          <p style={{ color: "#ffffff" }}>
+            The current theme is {theme}
+          </p>
+        </div>
+        <button onClick={toggleTheme}>Toggle Theme</button> */}
       </Router>
     </div>
   );
